fbc9f3639d99f1aa41e1e5687442a304af618a1b
Marko Arsic <marsicdev@gmail.com>
2017-09-19 00:17:01 +0200

    Add basic for scripts

---
+++ bit/intro.js
@@ -0,0 +1,48 @@
+
+var tekst = "We are learning \"JavaScript \"."
+document.write(tekst)
+
+var variable = 11
+variable = "JS Basics";
+
+x = "Combining types " + 11;
+y = 11 + " th";
+
+1.1234
+.1E23
+- 1.1E12
+2E-10
+
+var partOne = "Today is a";
+var partTwo = "nice day";
+var text = partOne + partTwo;
+
+console.log(text);
+
+var partOne = "Today is a";
+var partTwo = "nice day";
+var text = partOne + " " + partTwo;
+
+console.log(text);
+
+var isTropic = true;
+// variable = condition ? value1 : value2
+var fruit = isTropic ? "Bannana" : "Apple"
+
+console.log(fruit)
+
+// Kontrolne strukture i petlje
+document.writeln("Pocetak!<br>");
+
+{
+   document.writeln("Is this working? ");
+   document.writeln("YES it is!<br>");
+}
+
+document.write("Test");
+document.writeln('Test');
+
+if (condition) {
+   // Code inside will be executed to only
+   // if condition is true
+}
