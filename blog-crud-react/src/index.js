182b67872ec94b77801c2b7e927d412a2f224527
Marko Arsic <marsicdev@gmail.com>
2017-11-13 10:29:56 +0100

    Add new post form

--- blog-crud-react/src/index.js
+++ blog-crud-react/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import promise from "redux-promise";
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from "react-router-dom";
-import promise from "redux-promise";
+import { BrowserRouter, browserHistory, } from "react-router-dom";
 
 import App from './components/app';;
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 ReactDOM.render(
-  <Provider
-    store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
--- blog-crud-react/src/reducers/index.js
+++ blog-crud-react/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from 'redux-form';
+import { routerReducer } from 'react-router-redux';
 
 import PostsReducer from './posts-reducer'
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
-  form: formReducer
+  form: formReducer,
+  routing: routerReducer
 });
 
 export default rootReducer;
